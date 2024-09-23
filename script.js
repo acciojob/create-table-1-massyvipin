8a1353c46b3b260e0e51741d3b9e892d6394da3f
massyvipin <166917148+massyvipin@users.noreply.github.com>
2024-09-23 21:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    const table = document.getElementById("sampleTable");
+	let newRow = table.insertRow(0);
+	
+	let cell1 = newRow.insertCell(0);
+	cell1.innerHTMT = "New Cell1";
+	
+	let cell2 = newRow.insertCell(1);
+	cell2.innerHTMT = "New Cell2";
 }
